ba23d05f006abc3cf1502e802e7b1ab541447831
akshit sanwal <asancool007@gmail.com>
2021-04-13 02:21:38 +0530

    my sixth commit

---
+++ s4.js
@@ -0,0 +1,9 @@
+button=document.querySelector(".btn")
+search=document.querySelector(".search")
+input=document.querySelector(".input")
+
+button.addEventListener("click",function()
+{
+      search.classList.toggle("active")
+    
+})
